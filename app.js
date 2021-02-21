1e573ee88f531f81b30e88640115274e8d179542
TienNguyen <ngoctiennguyen155@gmail.com>
2021-02-21 21:25:23 +0700

    test

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors')
+const slowDown = require('express-slow-down')
+
+const app = express()
+app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 100, // allow 100 requests per 15 minutes, then...
+  delayMs: 500, // begin adding 500ms of delay per request above 100:
+  maxDelayMs: 60 *1000,
+  // request # 101 is delayed by  500ms
+  // request # 102 is delayed by 1000ms
+  // request # 103 is delayed by 1500ms
+  // etc.
+});
+app.use(cors())
+
+app.get("/",speedLimiter,(req,res)=>{
+    res.status(200).json({message:"ok"})
+})
+
+app.listen(process.env.PORT || 3000);
